a61e1a81ae15a9e6ec3f32bb8b102c97eb4f4eed
hatanlinh13 <hatanlinh13@gmail.com>
2025-10-19 13:11:45 +0700

    nvim: enable lsp for C++, Rust, Typescript, Python and Go

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -733,17 +733,17 @@ require("lazy").setup({
 				--
 				--  Feel free to add/remove any LSPs here that you want to install via Mason. They will automatically be installed and setup.
 				mason = {
-					-- clangd = {},
-					-- gopls = {},
-					-- pyright = {},
-					-- rust_analyzer = {},
+					clangd = {},
+					gopls = {},
+					pyright = {},
+					rust_analyzer = {},
 					-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 					--
 					-- Some languages (like typescript) have entire language plugins that can be useful:
 					--    https://github.com/pmizio/typescript-tools.nvim
 					--
 					-- But for many setups, the LSP (`ts_ls`) will work just fine
-					-- ts_ls = {},
+					ts_ls = {},
 					--
 					lua_ls = {
 						-- cmd = { ... },
