be6429ad34b3ba19fbd777817bd1e9bfa95904f1
hatanlinh <linh@fseek.net>
2026-02-09 00:50:31 +0700

    fish: use fastfetch as fetch tool and init zoxide

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set -U fish_greeting
+
     alias ssh-github="ssh-add $HOME/.ssh/github"
+    alias fetch="fastfetch"
+
+    zoxide init fish | source
 end
