220b6e08cb6e1fcb419222ad13eb8895d297958c
hatanlinh <linh@fseek.net>
2026-02-12 20:15:37 +0700

    fish: add commented out steps to allow run zellij on start up

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,4 +6,9 @@ if status is-interactive
     alias fetch="fastfetch"
 
     zoxide init fish | source
+
+    # For auto starting zellij
+    #set ZELLIJ_AUTO_ATTACH true
+    #set ZELLIJ_AUTO_EXIT true
+    #eval (zellij setup --generate-auto-start fish | string collect)
 end
