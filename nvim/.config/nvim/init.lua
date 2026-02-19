6a99b9ec51c60f4e0b89a341b51d4301567e484d
hatanlinh <linh@fseek.net>
2026-02-19 20:19:40 +0700

    nvim: disable autoimport for rust_analyzer

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -736,7 +736,17 @@ require("lazy").setup({
           clangd = {},
           -- gopls = {},
           pyright = {},
-          rust_analyzer = {},
+          rust_analyzer = {
+            settings = {
+              ['rust-analyzer'] = {
+                completion = {
+                  autoimport = {
+                    enable = false,
+                  }
+                }
+              }
+            }
+          },
           -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
           --
           -- Some languages (like typescript) have entire language plugins that can be useful:
